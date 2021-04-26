5dbc556d382d8aa097e46f637a47e0c6b99a9b2d
Popovycha <artem220397@gmail.com>
2021-04-26 16:05:48 -0400

    Revert "initial commit"
    This reverts commit cfd0b879

--- src/main/java/com/popovych/doctor/list/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.popovych.doctor.list.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message); //will pass the message
-    }
-}
